eb95958acbedc9f9cd986afb37db6a065836c037
Will Leverone <willminecraft34@gmail.com>
2018-03-12 13:02:59 -0400

    Actually works now

--- cuberoot.py
+++ cuberoot.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 import sys
 import getfactors
+import common
 
-#Nothing to see here yet
+def getCubeRoot(num):
+    if num >= 0:
+        return num ** (1. / 3.)
+    return -(-num) ** (1. / 3.)
+
+
+if __name__ == "__main__":
+    print(str(common.validateInteger(sys.argv[1])))
