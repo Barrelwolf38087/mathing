b9a315509f7350cafe2a5a6d4007d952c20ce6fe
Will Leverone <willminecraft34@gmail.com>
2018-03-17 17:55:40 -0400

    Fixed a thing.

--- gcf.py
+++ gcf.py
@@ -2,14 +2,13 @@
 #Experimenting with new import statements to save a bit of memory
 from sys import argv
 from math import gcd
-from common import validateInteger
+from common import validateIntegers
 
 def getGcf(*args):
     thisGcf = gcd(args[0], args[1])
     for i in args[2:]:
         thisGcf = gcd(thisGcf, i)
-    print(thisGcf)
-    
+    return thisGcf
 
 if __name__ == "__main__":
-    print(str(getGcf(validateInteger(argv))))
+    print(str(getGcf(*validateIntegers(argv))))
