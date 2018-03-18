296884ab64595b9942a540d8ed6e330b09725c65
Will Leverone <willminecraft34@gmail.com>
2018-03-17 22:26:41 -0400

    Optional rounding added

--- root.py
+++ root.py
@@ -3,18 +3,29 @@ from sys import argv
 from common import validateInteger
 from common import validateFloat
 
-def getRoot(root, radicand):
+def getRoot(root, radicand, perfect=False):
     if radicand >= 0:
-        return radicand ** (1. / validateFloat(root))
-    return -(-radicand) ** (1. / validateFloat(root))
+        result = radicand ** (1. / validateFloat(root))
+    else:
+        result = -(-radicand) ** (1. / validateFloat(root))
 
+    if perfect:
+        return round(result)
+    else:
+        return result
 
 if __name__ == "__main__":
     try:
         root = validateInteger(argv[1])
         radicand = validateInteger(argv[2])
-    
-        print(str(getRoot(root, radicand)))
 
     except IndexError:
-        print("Usage: root.py root radicand")
+        print("Usage: root.py root radicand [perfect]")
+
+    try:
+        perfect = True if argv[3].lower() == "perfect" else False
+    except IndexError:
+        perfect = False
+
+    print(str(getRoot(root, radicand, perfect)))
+
